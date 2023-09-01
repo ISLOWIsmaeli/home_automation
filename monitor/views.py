c59918ea78c69c880df70a5ae547f6d259273ea6
Ismael126895 <lenojore67@gmail.com>
2023-09-01 13:09:51 +0300

    changes to html file,mqtt folder...

--- monitor/views.py
+++ monitor/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from mqtt import listener
 # Create your views here.
+# def index(request):
+#     return HttpResponse("I am working")
+def home(response):
+    return render(response, "monitor/home.html",{})
