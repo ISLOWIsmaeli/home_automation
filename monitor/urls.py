7f73f4ed0b0f9cb2de5ffbc2d6a2c62112c6707d
Ismael126895 <lenojore67@gmail.com>
2023-09-13 10:10:57 +0300

    updated toggle LED status

--- monitor/urls.py
+++ monitor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns=[
+urlpatterns = [
     # path("",views.index ,name="index"),
-    path("",views.home,name="home")
+    path("", views.home, name="home")
 ]
